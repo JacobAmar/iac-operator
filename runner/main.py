4c8d90c5838be7bf72a76c626900c8b82b60f110
JacobAmar <jacobamar8@gmail.com>
2023-07-07 17:27:20 +0300

    test

--- runner/main.py
+++ runner/main.py
@@ -3,6 +3,8 @@ from git import Repo
 import json
 import logging
 from helpers.git import git_handler
+from helpers.terraform import main
+
 logging.basicConfig(level=logging.INFO)
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
@@ -10,11 +12,16 @@ v1 = client.CoreV1Api()
 crd = client.CustomObjectsApi()
 #crd.list_cluster_custom_object('terraform.iac.operator','v1','tfprojects')
 watch = watch.Watch()
-for event in watch.stream(crd.list_cluster_custom_object,'terraform.iac.operator','v1','tfprojects'):
+resource_version = ""
+for event in watch.stream(crd.list_cluster_custom_object,'terraform.iac.operator','v1','tfprojects',resource_version=resource_version):
     repository = event["object"]["spec"]["repository"]
+    terraform_directory = event["object"]["spec"]["path"]
+    terraform_variables = event["object"]["spec"]["variables"]
     project_name = event["object"]["metadata"]["name"]
     repo_directory = "cloned"
     event_type = event["type"]
-    if event_type == "ADDED":
+    resource_version = event["object"]["metadata"]["resourceVersion"]
+    if event_type == "ADDED" or event_type == "MODIFIED":
+        print(event_type)
         git_handler(repository=repository,path=project_name,branch="init")
-        print("done")
+        main(path=f"{project_name}/{terraform_directory}",variables=terraform_variables)
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,19 @@ resource "random_pet" "name" {
 
 locals {
     name = "${random_pet.name.id}-${var.region}"
+    cidr_blocks = { for key,val in var.vpc_cidrs: key => val }
 }
 
 variable "region" {
   type        = string
 }
 
+variable "vpc_cidrs" {}
+
 output "final" {
   value       = local.name
 }
 
+output "cidr_blocks" {
+  value       = local.cidr_blocks
+}
